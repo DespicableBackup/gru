92ba254510877dd7743272e43e330e109f5cd6ec
Greizgh <greizgh@ephax.org>
2017-11-29 21:23:26 +0100

    [build] Generate dev config in build script

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 use std::env;
+use std::fs::File;
+use std::path::Path;
+use std::io::Write;
 
 const CONFIG_PATH: Option<&str> = option_env!("GRU_CONFIG_ENV");
 
@@ -8,7 +11,13 @@ fn main() {
         if let Ok(profile) = env::var("PROFILE") {
             println!("cargo:rustc-cfg=build={:?}", profile);
             if profile == "debug" || profile == "test" {
-                // TODO: generate conf
+                if !Path::new("./dev.conf").exists() {
+                    let mut f = File::create("./dev.conf").unwrap();
+                    f.write_all(b"[database]
+path=test.db
+[ssh]
+pubkey=testpubkey").unwrap();
+                }
                 println!("cargo:rustc-env=GRU_CONFIG_PATH=./dev.conf");
             } else {
                 // Release conf
