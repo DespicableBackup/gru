31416e9b1826e59d21711b600bcd4e468cde39d4
Greizgh <greizgh@ephax.org>
2017-12-02 17:23:01 +0100

    Better config validation

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,12 @@ use ini::Ini;
 use std::fs::File;
 use std::io::Read;
 
+#[derive(Fail, Debug)]
+#[fail(display = "Invalid configuration: {}", message)]
+pub struct ConfigError {
+    message: String,
+}
+
 /// Gru configuration
 pub struct Config {
     /// Public key
@@ -11,21 +17,30 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn from_file(path: &str) -> Result<Config, ()> {
-        let conf = Ini::load_from_file(path).expect("opening config file");
+    pub fn from_file(path: &str) -> Result<Config, ConfigError> {
+        let conf = Ini::load_from_file(path).map_err(|e| ConfigError { message: e.msg })?;
 
-        let db_section = conf.section(Some("database")).unwrap();
-        let ssh_section = conf.section(Some("ssh")).unwrap();
+        let db_section = conf.section(Some("database"))
+            .ok_or_else(|| ConfigError { message: "missing database section".to_owned() })?;
+        let ssh_section = conf.section(Some("ssh"))
+            .ok_or_else(|| ConfigError { message: "missing ssh section".to_owned() })?;
 
         let mut pubkey = String::new();
         if let Some(key) = ssh_section.get("pubkey") {
             pubkey = key.to_owned();
         } else {
-            let mut file = File::open(ssh_section.get("pubkey-path").unwrap()).expect("load public key");
-            file.read_to_string(&mut pubkey).expect("read public key");
+            let mut file = File::open(
+                    ssh_section.get("pubkey-path")
+                    .ok_or_else(|| ConfigError { message: "missing pubkey config".to_owned() })?
+                )
+                .map_err(|_| ConfigError { message: "could not open public key file".to_owned() })?;
+            file.read_to_string(&mut pubkey)
+                .map_err(|_| ConfigError { message: "could not read public key".to_owned() })?;
         }
 
-        let db_path = db_section.get("path").unwrap().to_owned();
+        let db_path = db_section.get("path")
+            .ok_or_else(|| ConfigError { message: "missing database path".to_owned() })?
+            .to_owned();
 
         Ok(Config {
             pubkey: pubkey,
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ extern crate r2d2;
 extern crate r2d2_diesel;
 extern crate ini;
 #[macro_use] extern crate serde_derive;
+extern crate failure;
+#[macro_use] extern crate failure_derive;
 
 mod db;
 mod cli;
@@ -20,6 +22,8 @@ mod schema;
 mod server;
 mod manage_minions;
 mod config;
+
+use config::Config;
 
 embed_migrations!();
 
@@ -30,13 +34,21 @@ const CONFIG_PATH: &str = env!("GRU_CONFIG_PATH");
 fn main() {
     let matches = cli::get_app(APP_NAME, VERSION).get_matches();
 
-    let conf = config::Config::from_file(CONFIG_PATH).expect("loading config file");
+    let conf: Config;
+
+    match Config::from_file(CONFIG_PATH) {
+        Ok(c) => conf = c,
+        Err(e) => {
+            eprintln!("{}", e);
+            return;
+        }
+    }
 
     let pool = db::connect(&conf.db_path);
 
     let connection = pool.get().unwrap();
     if embedded_migrations::run(&*connection).is_err() {
-        println!("Unable to run database migration");
+        eprintln!("Unable to run database migration");
         return;
     }
 
